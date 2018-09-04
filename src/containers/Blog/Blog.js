1a2fe2701cc5babb618f4aae74360d9d156f1f5f
Anthony <anthonymarques@outlook.com>
2018-09-04 19:46:51 +0200

    setted up mapping and adding an attribute to pass

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -14,12 +14,19 @@ class Blog extends Component {
     componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts')
              .then(response => {
-                 this.setState({posts:response.data});
+                 const posts = response.data.slice(0,4);
+                 const updatedPosts = response.data.map(post=>{
+                     return{
+                         ...post,
+                         author: 'Tony'
+                     }
+                 });
+                 this.setState({posts:updatedPosts});
              })
     }
     render() {
         const posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title}/>
+            return <Post key={post.id} title={post.title} author={post.author}/>
         })
         return (
             <div>
