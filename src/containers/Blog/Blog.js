74151add76a678027bfa473a98fc3da29c91c4eb
Anthony <anthonymarques@outlook.com>
2018-09-16 21:08:29 +0200

    uploaded example

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,29 +1,31 @@
 import React, { Component } from 'react';
+// import axios from 'axios';
+import { Route, Link } from 'react-router-dom';
 
-import FullPost from '../Blog/FullPost/FullPost';
-import NewPost from '../Blog/NewPost/NewPost';
 import './Blog.css';
+import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
-
-    render() {
+    render () {
         return (
             <div className="Blog">
                 <header>
-                        <nav>
-                            <ul>
-                                <li><a href='/'>Home</a></li>
-                                <li><a href='/new-post'>New Post</a></li>
-                            </ul>
-                        </nav>
+                    <nav>
+                        <ul>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to={{
+                                pathname: '/new-post',
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>New Post</Link></li>
+                        </ul>
+                    </nav>
                 </header>
-                
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+                {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+                <Route path="/" render={() => <h1>Home 2</h1>} /> */}
+                <Route path="/" exact component={Posts} />
+                <Route path="/new-post" component={NewPost} />
             </div>
         );
     }
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,53 +1,56 @@
 import React, { Component } from 'react';
 import axios from '../../../axios';
 
-import Post from '../../components/Post/Post';
-import './Post.css';
+import Post from '../../../components/Post/Post';
+import './Posts.css';
 
-class post extends Componet {
+class Posts extends Component {
     state = {
         posts: []
     }
-    postSelectHandler = (id) => {
-        this.setState({ selectedPostId: id });
-    }
-    
-    componentDidMount() {
-        axios.get('/posts')
-            .then(response => {
+
+    componentDidMount () {
+        console.log(this.props);
+        axios.get( '/posts' )
+            .then( response => {
                 const posts = response.data.slice(0, 4);
                 const updatedPosts = posts.map(post => {
                     return {
                         ...post,
-                        author: 'Tony'
+                        author: 'Max'
                     }
                 });
-                this.setState({ posts: updatedPosts });
-            })
+                this.setState({posts: updatedPosts});
+                // console.log( response );
+            } )
             .catch(error => {
-                console.log(error)
-                // this.setState({ error: true });
-            })
+                console.log(error);
+                // this.setState({error: true});
+            });
     }
 
-    render() {
-        let posts = <p> Something went wrong</p>
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
+    }
+    
+    render () {
+        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
         if (!this.state.error) {
             posts = this.state.posts.map(post => {
-                return <Post
-                    key={post.id}
-                    title={post.title}
-                    author={posts.author}
-                    clicked={() => this.postSelectHandler(post.id)}
-                />
+                return <Post 
+                    key={post.id} 
+                    title={post.title} 
+                    author={post.author}
+                    clicked={() => this.postSelectedHandler(post.id)} />;
             });
         }
+
         return (
             <section className="Posts">
                 {posts}
             </section>
-        )
+        );
     }
 }
 
-export default post;
+export default Posts;
